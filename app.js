0f48c90be79c07282019616544d01590903972fd
Lawrence Ofoli Mensah <lawrenceofoli@gmail.com>
2024-06-26 13:55:02 +0000

    refactor node code

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const http= require('http');
-const  hostname= '0.0.0.0';
-const port =8000;
-const  Server = http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  
-  response.end("Congratulations on successfully deploying your Dockerized Node.js application!\n\nYour application is now up and running inside a Docker container,\nensuring a consistent and isolated environment. You can access your app at http://localhost:8000.\n\nWell done on achieving this milestone in your development journey. Happy coding! 🚀");
+const http = require("http");
+const port = 8000;
 
+const hurray =
+  "Congratulations on successfully deploying your Dockerized Node.js application!\n\nYour application is now up and running inside a Docker container,\nensuring a consistent and isolated environment. You can access your app at http://localhost:8000.\n\nWell done on achieving this milestone in your development journey. Happy coding! 🚀";
 
-});
-Server.listen(port, hostname,()=>{
- console.log('Server running at http://127.0.0.1:8000/');
+const test = (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(hurray);
+};
+const server = http.createServer(test);
 
-console.log("App deplied");
-            
-
-})
-
+server.listen(port, () =>
+  console.log(`Server running at http://127.0.0.1:${port}/`)
+);
